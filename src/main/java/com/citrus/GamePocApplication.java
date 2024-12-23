6400e6550ea911fe77b06e148159ef1ba78b41f9
evan01203394 <evan01203394@globalline.club>
2024-12-23 09:09:46 +0800

    first commit

---
+++ src/main/java/com/citrus/GamePocApplication.java
@@ -0,0 +1,14 @@
+package com.citrus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamePocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamePocApplication.class, args);
+		System.out.println("=============== START =============== ");
+	}
+
+}
